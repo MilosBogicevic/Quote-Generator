c4bc4ede378d4b712eccdcaa2ecfb266f5834ec5
MilosBogicevic <milbog.development@gmail.com>
2020-10-18 15:11:51 +0200

    CSS Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import style from './App.css'
+
+const API_URL = "https://type.fit/api/quotes"
 
 function App() {
   return (
     <div>
-      <h1>Hello, world.</h1>
+      <div className="container">
+        <h1>Quote text</h1>
+        <p>Author</p>
+        <button>NEW QUOTE</button>
+      </div>
     </div>
   )
 }

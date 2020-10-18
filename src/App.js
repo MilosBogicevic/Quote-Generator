aef623ba973355a6cbc7978ac7286190501faa8d
MilosBogicevic <milbog.development@gmail.com>
2020-10-18 16:38:46 +0200

    Fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import style from './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-const API_URL = "https://type.fit/api/quotes"
+const API_URL = "https://type.fit/api/quotes";
 
 function App() {
+  const [data, setData] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div>
       <div className="container">
-        <h1>Quote text</h1>
-        <p>Author</p>
-        <button>NEW QUOTE</button>
+        <h2>{data[index]?.text}</h2>
+        <p>{data[index]?.author}</p>
+        <button onClick={() => setIndex((prev) => prev + 1)}>NEW QUOTE</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

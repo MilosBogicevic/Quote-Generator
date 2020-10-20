6b86c2d1f3c622d30540121ac8f7c5d8e9d49e38
MilosBogicevic <milbog.development@gmail.com>
2020-10-20 17:25:46 +0200

    Replace author missing info with unknown.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import "./App.css";
 const API_URL = "https://type.fit/api/quotes";
 
 function App() {
-  const [data, setData] = useState("");
+  const [quote, setQuote] = useState("");
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((quote) => setQuote(quote));
   }, []);
 
   return (
     <div>
       <div className="container">
-        <h2>{data[index]?.text}</h2>
-        <p>{data[index]?.author}</p>
-        <button onClick={() => setIndex((prev) => prev + 1)}>NEW QUOTE</button>
+        <h2>{quote[index]?.text}</h2>
+        <p>{quote[index]?.author ?? "Uknown"}</p>
+        <button
+          onClick={() => setIndex(() => Math.floor(Math.random() * 200) + 1)}
+        >
+          NEW QUOTE
+        </button>
       </div>
     </div>
   );
